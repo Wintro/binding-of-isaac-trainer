24994e99e9598e109dbb5b3917e93583ee60287e
Wintro <ziyad.edher@gmail.com>
2017-04-01 11:27:52 +0300

    initial commit

---
+++ .templates/HPP.hpp
@@ -0,0 +1,38 @@
+/* FILE START */
+
+/*/
+ * This file is part of The Binding of Isaac Trainer.
+ *
+ * The Binding of Isaac Trainer is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The Binding of Isaac Trainer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with The Binding of Isaac Trainer. If not, see <http://www.gnu.org/licenses/>.
+ *
+/*/
+
+/* <HEADER FOR> */
+
+/* HEADER START */
+
+
+#ifndef PATH_TO_FILE_NAME_H
+#define PATH_TO_FILE_NAME_H
+
+
+
+
+
+#endif /* PATH_TO_FILE_NAME_H */
+
+
+/* HEADER END */
+
+/* FILE END */
